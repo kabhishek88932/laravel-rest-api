dd598b86b78fec64617a57a6c516a38c52a43338
Abhishek Kumar <abhishek.kumar@thinkwidetech.com>
2025-04-09 12:02:29 +0530

    commit initial

--- app/Mail/PostCreatedMail.php
+++ app/Mail/PostCreatedMail.php
@@ -3,63 +3,30 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class PostCreatedMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $post;
+
     /**
      * Create a new message instance.
-     *
-     * @return void
      */
-    public $post;
-    public function __construct()
+    public function __construct($post)
     {
         $this->post = $post;
     }
 
+    /**
+     * Build the message.
+     */
     public function build()
     {
-    return $this->markdown('emails.post.created')->subject('New Post Created');
+        return $this->markdown('emails.post.created')
+                    ->with(['post' => $this->post]);
     }
-
-    /**
-     * Get the message envelope.
-     *
-     * @return \Illuminate\Mail\Mailables\Envelope
-     */
-    public function envelope()
-    {
-        return new Envelope(
-            subject: 'Post Created Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     *
-     * @return \Illuminate\Mail\Mailables\Content
-     */
-    public function content()
-    {
-        return new Content(
-            markdown: 'emails.post.created',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array
-     */
-    public function attachments()
-    {
-        return [];
-    }
+    
 }
